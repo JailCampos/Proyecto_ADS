1a6c225fd902a69a127020e2f4933d500bfa5a85
jailcampos <jailantac@gmail.com>
2021-06-18 14:39:48 -0500

    tres

--- proyecto_ADS/Proyecto_ADS/src/main/java/gui/FrmCUN1.java
+++ proyecto_ADS/Proyecto_ADS/src/main/java/gui/FrmCUN1.java
@@ -20,6 +20,11 @@ import javax.swing.DropMode;
 import javax.swing.border.TitledBorder;
 import javax.swing.UIManager;
 import javax.swing.JComboBox;
+import javax.swing.border.BevelBorder;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.border.LineBorder;
 
 public class FrmCUN1 extends JFrame implements ActionListener {
 
@@ -44,9 +49,13 @@ public class FrmCUN1 extends JFrame implements ActionListener {
 	private JButton btnNewButton_3;
 	private JButton btnNewButton_4;
 	private JButton btnNewButton_5;
-	private JPanel panel_3;
 	private JTextField txtDesprictionOfRequirement;
 	private JButton btnNewButton;
+	private JPanel panel_4;
+	private JTextField textField_1;
+	private JTextField textField_2;
+	private JTable table;
+	private final JPanel panel_5 = new JPanel();
 
 	/**
 	 * Launch the application.
@@ -71,7 +80,7 @@ public class FrmCUN1 extends JFrame implements ActionListener {
 		setIconImage(Toolkit.getDefaultToolkit().getImage(FrmCUN1.class.getResource("/icon/lista-de-verificacion (1).png")));
 		setTitle("Generar Tramite Financiero");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 1156, 1004);
+		setBounds(100, 100, 1156, 822);
 		contentPane = new JPanel();
 		contentPane.setBackground(Color.DARK_GRAY);
 		contentPane.setForeground(Color.LIGHT_GRAY);
@@ -79,17 +88,10 @@ public class FrmCUN1 extends JFrame implements ActionListener {
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		JLabel lblCdigo = new JLabel("Requerimiento N° 0001 - 2021");
-		lblCdigo.setFont(new Font("Tahoma", Font.PLAIN, 30));
-		lblCdigo.setForeground(Color.WHITE);
-		lblCdigo.setBackground(Color.WHITE);
-		lblCdigo.setBounds(36, 27, 461, 64);
-		contentPane.add(lblCdigo);
-		
 		JPanel panel = new JPanel();
 		panel.setBackground(Color.DARK_GRAY);
-		panel.setBorder(new TitledBorder(new TitledBorder(UIManager.getBorder("TitledBorder.border"), "Representative Details", TitledBorder.LEADING, TitledBorder.TOP, null, new Color(192, 192, 192)), "", TitledBorder.LEFT, TitledBorder.TOP, null, Color.WHITE));
-		panel.setBounds(25, 123, 527, 267);
+		panel.setBorder(new TitledBorder(UIManager.getBorder("TitledBorder.border"), "Representative Details", TitledBorder.LEADING, TitledBorder.TOP, null, Color.WHITE));
+		panel.setBounds(25, 123, 522, 267);
 		contentPane.add(panel);
 		panel.setLayout(null);
 		
@@ -218,44 +220,116 @@ public class FrmCUN1 extends JFrame implements ActionListener {
 		btnNewButton_2.setBounds(459, 18, 50, 38);
 		panel_2.add(btnNewButton_2);
 		
-		lblJuly = new JLabel("July 18, 2021 ");
-		lblJuly.setForeground(Color.WHITE);
-		lblJuly.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		lblJuly.setBounds(40, 78, 152, 34);
-		contentPane.add(lblJuly);
-		
 		btnNewButton_3 = new JButton("");
+		btnNewButton_3.setIcon(new ImageIcon(FrmCUN1.class.getResource("/icon/cerrar.png")));
 		btnNewButton_3.setForeground(new Color(0, 128, 128));
 		btnNewButton_3.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		btnNewButton_3.setBackground(new Color(0, 206, 209));
-		btnNewButton_3.setBounds(410, 633, 50, 38);
+		btnNewButton_3.setBounds(362, 670, 95, 38);
 		contentPane.add(btnNewButton_3);
 		
 		btnNewButton_4 = new JButton("");
+		btnNewButton_4.setIcon(new ImageIcon(FrmCUN1.class.getResource("/icon/editar.png")));
 		btnNewButton_4.setForeground(new Color(0, 128, 128));
 		btnNewButton_4.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		btnNewButton_4.setBackground(new Color(0, 206, 209));
-		btnNewButton_4.setBounds(254, 633, 50, 38);
+		btnNewButton_4.setBounds(221, 670, 95, 38);
 		contentPane.add(btnNewButton_4);
 		
 		btnNewButton_5 = new JButton("");
+		btnNewButton_5.setIcon(new ImageIcon(FrmCUN1.class.getResource("/icon/signo-mas-para-agregar.png")));
 		btnNewButton_5.setForeground(new Color(0, 128, 128));
 		btnNewButton_5.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		btnNewButton_5.setBackground(new Color(0, 206, 209));
-		btnNewButton_5.setBounds(89, 633, 50, 38);
+		btnNewButton_5.setBounds(82, 670, 95, 38);
 		contentPane.add(btnNewButton_5);
 		
-		panel_3 = new JPanel();
-		panel_3.setBackground(Color.DARK_GRAY);
-		panel_3.setForeground(Color.LIGHT_GRAY);
-		panel_3.setBorder(new TitledBorder(null, "Operation you want to perform", TitledBorder.LEADING, TitledBorder.TOP, null, Color.LIGHT_GRAY));
-		panel_3.setBounds(473, 766, 519, 70);
-		contentPane.add(panel_3);
-		panel_3.setLayout(null);
+		JPanel panel_1 = new JPanel();
+		panel_1.setBorder(new TitledBorder(null, "Details", TitledBorder.LEADING, TitledBorder.TOP, null, Color.WHITE));
+		panel_1.setBackground(new Color(0, 206, 209));
+		panel_1.setForeground(new Color(0, 191, 255));
+		panel_1.setBounds(562, 129, 585, 488);
+		contentPane.add(panel_1);
+		panel_1.setLayout(null);
 		
-		JComboBox comboBox = new JComboBox();
-		comboBox.setBounds(16, 19, 427, 36);
-		panel_3.add(comboBox);
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setViewportBorder(new LineBorder(new Color(0, 0, 0)));
+		scrollPane.setBounds(8, 24, 550, 457);
+		panel_1.add(scrollPane);
+		
+		table = new JTable();
+		table.setModel(new DefaultTableModel(
+			new Object[][] {
+			},
+			new String[] {
+				"ID_OCF", "ID_Representative", "Names", "Email", "Direction", "Description"
+			}
+		));
+		table.getColumnModel().getColumn(1).setPreferredWidth(109);
+		table.getColumnModel().getColumn(2).setPreferredWidth(111);
+		table.getColumnModel().getColumn(3).setPreferredWidth(102);
+		table.getColumnModel().getColumn(4).setPreferredWidth(93);
+		table.getColumnModel().getColumn(5).setPreferredWidth(128);
+		scrollPane.setViewportView(table);
+		
+		panel_4 = new JPanel();
+		panel_4.setBounds(562, 630, 568, 139);
+		contentPane.add(panel_4);
+		panel_4.setLayout(null);
+		panel_4.setForeground(new Color(0, 191, 255));
+		panel_4.setBorder(new TitledBorder(UIManager.getBorder("TitledBorder.border"), "Quantity of requirements", TitledBorder.LEADING, TitledBorder.TOP, null, new Color(255, 255, 255)));
+		panel_4.setBackground(Color.DARK_GRAY);
+		
+		textField_1 = new JTextField();
+		textField_1.setForeground(Color.LIGHT_GRAY);
+		textField_1.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		textField_1.setColumns(10);
+		textField_1.setBackground(Color.DARK_GRAY);
+		textField_1.setBounds(91, 53, 152, 38);
+		panel_4.add(textField_1);
+		
+		textField_2 = new JTextField();
+		textField_2.setForeground(Color.LIGHT_GRAY);
+		textField_2.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		textField_2.setColumns(10);
+		textField_2.setBackground(Color.DARK_GRAY);
+		textField_2.setBounds(372, 53, 152, 38);
+		panel_4.add(textField_2);
+		
+		JLabel lblTotal = new JLabel("Total:");
+		lblTotal.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		lblTotal.setForeground(Color.LIGHT_GRAY);
+		lblTotal.setBounds(23, 53, 74, 36);
+		panel_4.add(lblTotal);
+		
+		JLabel lblQuantity = new JLabel("Quantity:");
+		lblQuantity.setForeground(Color.LIGHT_GRAY);
+		lblQuantity.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		lblQuantity.setBounds(276, 53, 103, 36);
+		panel_4.add(lblQuantity);
+		panel_5.setBorder(new LineBorder(new Color(0, 0, 0)));
+		panel_5.setBackground(new Color(0, 206, 209));
+		panel_5.setBounds(-7, 27, 1154, 90);
+		contentPane.add(panel_5);
+		panel_5.setLayout(null);
+		
+		lblJuly = new JLabel("July 18, 2021 ");
+		lblJuly.setBounds(31, 51, 152, 34);
+		panel_5.add(lblJuly);
+		lblJuly.setForeground(Color.WHITE);
+		lblJuly.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		
+		JLabel lblCdigo = new JLabel("Requerimiento N° 0001 - 2021");
+		lblCdigo.setBounds(27, 0, 461, 64);
+		panel_5.add(lblCdigo);
+		lblCdigo.setFont(new Font("Tahoma", Font.PLAIN, 30));
+		lblCdigo.setForeground(Color.WHITE);
+		lblCdigo.setBackground(Color.WHITE);
+		
+		JLabel lblF = new JLabel("");
+		lblF.setBounds(1060, 10, 72, 71);
+		panel_5.add(lblF);
+		lblF.setIcon(new ImageIcon(FrmCUN1.class.getResource("/icon/contract.png")));
 	}
 
 	public void actionPerformed(ActionEvent e) {
@@ -266,5 +340,4 @@ public class FrmCUN1 extends JFrame implements ActionListener {
 	protected void do_btnNewButton_actionPerformed(ActionEvent e) {
 
 	}
-	
 }
