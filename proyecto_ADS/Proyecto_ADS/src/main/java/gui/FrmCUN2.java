99d3c9cd0633f9374fceffaa4dfa13cc1380e73d
Alvaro Moran <alvaro.159mor@gmail.com>
2021-06-18 14:49:50 -0500

    commit

--- proyecto_ADS/Proyecto_ADS/src/main/java/gui/FrmCUN2.java
+++ proyecto_ADS/Proyecto_ADS/src/main/java/gui/FrmCUN2.java
@@ -6,6 +6,7 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
 
 public class FrmCUN2 extends JFrame {
 
@@ -14,6 +15,7 @@ public class FrmCUN2 extends JFrame {
 	 */
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
+	private JButton btnPvto;
 
 	/**
 	 * Launch the application.
@@ -39,8 +41,11 @@ public class FrmCUN2 extends JFrame {
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		btnPvto = new JButton("pvto el que lo presione");
+		btnPvto.setBounds(188, 85, 193, 71);
+		contentPane.add(btnPvto);
 	}
-
 }
